62bf0cdec8e67e85781d457e902117df864f03fe
Jaime <andres_angel@live.com.ar>
2023-11-07 17:27:34 -0500

    Carga inicial de la pagina

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react"
+import FirstComponent from "./components/FirstComponent"
+import "./styles.css"
 
 function App() {
+  const [products, setProducts] = useState([])
+  useEffect(() => {
+    console.log("Ejecutandose")
+    // consumo de la api de leer productos
+    fetch('https://test-server-z9m2.onrender.com/getProducts', {
+      method: "GET"
+    }).then(response => response.json()
+      .then(data => setProducts(data.products))
+      .catch(err => console.log(err))
+    )
+      .catch(errorResponse => console.log(errorResponse))
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <FirstComponent></FirstComponent>
+      <FirstComponent />
+      {products.length === 0 ?
+      <h3>No hay productos registrados</h3>:
+      products.map(producto => <div key={producto.id} className="fondo">
+      <h3>ID: {producto.id}</h3>
+      <p>Producto: {producto.nombre}</p>
+      <p>{producto.precio}</p>
+      <p>{producto.cantidad}</p>
+      </div>)
+      }
+    </>
   );
 }
 
---
+++ src/components/FirstComponent.js
@@ -0,0 +1,22 @@
+import { useState } from "react"
+
+const FirstComponent = () => {
+    const [cont, setCont] = useState(0)
+
+    const add = () => {
+        setCont(cont + 1)
+    }
+
+    return (
+        <>
+            <h1>Mi proyecto e-commerce</h1>
+            <h2>Contador: {cont}</h2>
+            <button onClick={add}>Aumentar</button>
+            <button onClick={() => {
+                setCont(cont - 1)
+            }}>Disminuir</button>
+        </>
+    )
+}
+
+export default FirstComponent
