fe806e5e973afbe04283dd4f133af74623f95c27
Jaime <andres_angel@live.com.ar>
2023-11-07 17:45:00 -0500

    se agrega react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react"
+import { HashRouter, Routes, Route } from "react-router-dom"
 import FirstComponent from "./components/FirstComponent"
 import "./styles.css"
 
@@ -16,19 +17,26 @@ function App() {
       .catch(errorResponse => console.log(errorResponse))
   }, [])
   return (
-    <>
-      <FirstComponent></FirstComponent>
-      <FirstComponent />
-      {products.length === 0 ?
-      <h3>No hay productos registrados</h3>:
-      products.map(producto => <div key={producto.id} className="fondo">
-      <h3>ID: {producto.id}</h3>
-      <p>Producto: {producto.nombre}</p>
-      <p>{producto.precio}</p>
-      <p>{producto.cantidad}</p>
-      </div>)
-      }
-    </>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<>
+          <FirstComponent></FirstComponent>
+          <FirstComponent />
+          {products.length === 0 ?
+            <h3>No hay productos registrados</h3> :
+            products.map(producto => <div key={producto.id} className="fondo">
+              <h3>ID: {producto.id}</h3>
+              <p>Producto: {producto.nombre}</p>
+              <p>{producto.precio}</p>
+              <p>{producto.cantidad}</p>
+            </div>)
+          }
+        </>} />
+        <Route path="*" element={<h1>Pagina no encontrada</h1>} />
+      </Routes>
+
+
+    </HashRouter>
   );
 }
 
